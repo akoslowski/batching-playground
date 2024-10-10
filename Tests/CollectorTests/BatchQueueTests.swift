802d2263236a5b6adc324d438607c51f3b7990c2
akoslowski <2005824+akoslowski@users.noreply.github.com>
2024-10-10 12:37:38 +0200

    Move unrelated tests into separate file

--- Tests/CollectorTests/BatchQueueTests.swift
+++ Tests/CollectorTests/BatchQueueTests.swift
@@ -4,45 +4,6 @@ import AsyncAlgorithms
 
 @testable import Collector
 
-struct ExecuteAfterTimeout {
-
-    private let task: Task<Void, Never>
-
-    init(of timeout: Duration, action: @escaping @Sendable () throws -> Void) {
-        task = Task {
-            do {
-                try await Task.sleep(for: timeout)
-                try action()
-            }
-            catch {}
-        }
-    }
-
-    func cancel() {
-        task.cancel()
-    }
-}
-
-@Test func simpleQueueWithAsyncChannel() async throws {
-    let channel = AsyncChannel<String>()
-
-    let timeout = ExecuteAfterTimeout(of: .milliseconds(5)) {
-        channel.finish()
-    }
-
-    /// Sends an element to an **awaiting** iteration.
-    async let _ = Task {
-        await channel.send("hello")
-    }
-
-    var values: [String] = []
-    for await value in channel {
-        values.append(value)
-    }
-
-    #expect(values == ["hello"])
-}
-
 @Test func testChunksOfBatches() async throws {
     let q = StreamQueue<String>()
 
