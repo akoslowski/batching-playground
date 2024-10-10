4ca2af6a56f89f1123c6b88cf54efbaadbc65ea0
akoslowski <2005824+akoslowski@users.noreply.github.com>
2024-10-10 12:45:46 +0200

    Make it simpler

--- Tests/CollectorTests/OtherTests.swift
+++ Tests/CollectorTests/OtherTests.swift
@@ -2,34 +2,16 @@ import Foundation
 import Testing
 import AsyncAlgorithms
 
-struct ExecuteAfterTimeout {
-
-    private let task: Task<Void, Never>
-
-    init(of timeout: Duration, action: @escaping @Sendable () throws -> Void) {
-        task = Task {
-            do {
-                try await Task.sleep(for: timeout)
-                try action()
-            }
-            catch {}
-        }
-    }
-
-    func cancel() {
-        task.cancel()
-    }
-}
-
 @Test func simpleQueueWithAsyncChannel() async throws {
     let channel = AsyncChannel<String>()
 
-    let _ = ExecuteAfterTimeout(of: .milliseconds(5)) {
+    Task {
+        try await Task.sleep(for: .milliseconds(1))
         channel.finish()
     }
 
     /// Sends an element to an **awaiting** iteration.
-    async let _ = Task {
+    Task {
         await channel.send("hello")
     }
 
